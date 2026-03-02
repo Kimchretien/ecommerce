0a302e64ff2586fa2a2055078024468b1432cb7b
Kimchretien <kimintsinzi@gmail.com>
2026-03-02 16:50:58 +0200

    integration de stripe payement

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:ecommerce/pages/redirection_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce/firebase_options.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+   Stripe.publishableKey = "pk_test_51T4KyJBjojLkMY5ejDnPdMgCdWpIW45KI2AFfQe7kUP4Z8X15T6YlwehY6YNRTE0khzwJK4YKr9I0ILa0H6cJTHS00VMJ38Cls";
+
+  await Stripe.instance.applySettings();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
